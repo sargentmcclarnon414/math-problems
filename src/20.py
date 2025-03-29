185736804e7a47c4fcd231183e2915bdef475f07
sargentmcclarnon414 <sargentmcclarnon414@gmail.com>
2025-03-29 20:12:46 +0000

    Add 20.py

---
+++ src/20.py
@@ -0,0 +1,9 @@
+# This is a placeholder code to start your math problem solving journey.
+# Feel free to add or modify it according to the specific type of math problem you are trying to solve.
+# Here's an example for addition:
+
+# x + 3 = 10
+
+# To find the value of 'x':
+x = 10 - 3
+print(x)
