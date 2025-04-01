b650acecc1f24bf155d047505cbb0a190c5ee2ef
sargentmcclarnon414 <sargentmcclarnon414@gmail.com>
2025-04-01 15:10:57 +0000

    Add 23.py

---
+++ src/23.py
@@ -0,0 +1,7 @@
+def calculate_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+radius = 5 # Example value for radius
+area = calculate_area(radius)
+print("The area of a circle with radius", radius, "is:", area)
