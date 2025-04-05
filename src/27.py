61e90e7ac23c1cc6f7b79c70c4b1a76e11c46579
sargentmcclarnon414 <sargentmcclarnon414@gmail.com>
2025-04-05 06:01:23 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,14 @@
+def calculate_area(width: float, height: float) -> float:
+    """
+    Calculate the area of a rectangle.
+    
+    Args:
+        width (float): The width of the rectangle.
+        height (float): The height of the rectangle.
+        
+    Returns:
+        float: The area of the rectangle.
+    """
+    return width * height
+
+calculate_area(5.0, 3.0)
